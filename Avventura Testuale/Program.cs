46d95700d35a1fcc4abf63ca91b44088e036b624
Angelo Corrao <precipitevoliss1@gmail.com>
2022-10-24 12:57:34 +0200

    Inizializzazione struttura

---
+++ Avventura Testuale/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Avventura_Testuale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
